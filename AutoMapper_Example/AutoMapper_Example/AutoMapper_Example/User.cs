a9a61742773343c7112cde7c486eb4e085e74143
spacholski1225 <spacholski1225@gmail.com>
2021-07-09 15:08:53 +0200

    created dto and model

---
+++ AutoMapper_Example/AutoMapper_Example/AutoMapper_Example/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapper_Example
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Adress { get; set; }
+        public string City { get; set; }
+    }
+}
